6817a42b14695650c912f893d7ef5589e6431629
Mariano Córdoba <marianocordoba039@gmail.com>
2025-02-10 22:09:17 -0300

    fix: remove unused env vars

--- src/env.ts
+++ src/env.ts
@@ -5,7 +5,5 @@ export const env = type({
   NEXT_PUBLIC_APP_NAME: 'string',
   NEXT_PUBLIC_APP_SHORT_NAME: 'string',
   NEXT_PUBLIC_APP_DESCRIPTION: 'string',
-  NEXT_PUBLIC_VAPID_PUBLIC_KEY: 'string',
-  VAPID_PRIVATE_KEY: 'string',
   DATABASE_URL: 'string.url',
 }).assert(process.env)
